a469be5173cbb7b76fdcb587a38cb607a28abaf4
Rita888 <291761310@qq.com>
2019-05-13 00:19:58 +0800

    20190512 update

---
+++ WebScanner/GUI/13.py
@@ -0,0 +1,61 @@
+#--Created by WD
+#python 3.6
+#coding:utf-8
+
+# 本脚本用于制作各等级漏洞统计柱状图
+
+
+def set_ch():
+    '''设置中文无法显示的问题'''
+
+    from pylab import mpl
+    mpl.rcParams['font.sans-serif'] = ['FangSong']  # 指定默认字体
+    mpl.rcParams['axes.unicode_minus'] = False  # 解决保存图像是负号显示为方块的问题
+
+
+def make_plotjpg():
+    '''绘制漏洞统计树状图并保存成图片'''
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    set_ch()  # 设置中文正常显示
+
+    # 从键盘输入列表
+    # data = input('请输入漏洞个数，按照从漏洞级别从低到高（以逗号隔开）:')
+    # height = input(u'请输入柱状图纵坐标高度：')
+    # span = input(u'请输入柱状图纵坐标间隔：')
+    data = (1,2,4,6,7)
+    height = 8
+    span = 2
+    print(data,height,span)
+    print(len(data))
+
+    # 设置每根柱子的颜色
+    colors = ['forestgreen', 'mediumblue', 'gold', 'r', 'maroon']
+
+    # 绘制柱状图
+    plt.bar(np.arange(len(data)), data, color=colors)
+
+    # 设置柱状图的标题、横坐标标签、纵坐标标签
+    plt.title(u'漏洞个数统计')
+    plt.xlabel(u'漏洞类型')
+    plt.ylabel(u'个数')
+
+    # 添加横纵坐标的刻度
+    labels = [u'信息', u'低危', u'中危', u'高危', u'紧急']
+    plt.xticks(range(len(labels)), labels)
+    plt.yticks(np.arange(0, int(height), int(span)))
+    plt.savefig('11.png')
+
+    plt.show()
+    plt.close()
+
+
+def main():
+    make_plotjpg()
+
+
+if __name__ == '__main__':
+    main()
+
